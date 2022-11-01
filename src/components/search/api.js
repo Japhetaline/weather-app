8409fe48ed0580a85edd66b0e9874e068a47c3cd
Japhetaline <Japhetdamassoh@gmail.com>
2022-11-01 02:19:35 +0100

    first commit

---
+++ src/components/search/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'eee7a86c24msh3d674ed3444c14ep15b462jsn802642989650',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "337470edf3e96ff27855fc36525eaa23";
